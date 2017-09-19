77650a464a65b01574ba40093ceaa62f03212cd1
Sérgio Gonçalves <eu.sergiogoncalves@gmail.com>
2017-09-19 11:03:32 -0400

    Continuing the project

--- src/main/java/com/project/dataobject/BeerDO.java
+++ src/main/java/com/project/dataobject/BeerDO.java
@@ -1,12 +1,19 @@
 package com.project.dataobject;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class BeerDO {
 	
-	@NotBlank
+	@NotBlank(message = "SKU is required")
 	private String sku;
+	
+	@NotBlank(message = "Name is required")
 	private String name;
+	
+	@Size(min = 1, max = 50, message = "Size of description must to be between 1 and 50")
+	private String descricao;
 	
 	public String getSku() {
 		return sku;
@@ -22,6 +29,14 @@ public class BeerDO {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 }
